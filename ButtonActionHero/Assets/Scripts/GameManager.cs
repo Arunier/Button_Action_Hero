8769205d14ed2ac3faa53e58ff1fc232ea0e0e6f
Arunier <86580431+Arunier@users.noreply.github.com>
2022-06-06 04:02:17 +0900

    UI배치&GM 스크립트 추가
    UI배치랑, GameManager 추가함

---
+++ ButtonActionHero/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime; //생존 시간
+    private bool isGameover; //게임오버 상태
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        surviveTime = 0;
+        isGameover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time : " + surviveTime.ToString("F2");
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+    public void EndGame()
+    {
+        isGameover = true;
+        gameoverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if(surviveTime > bestTime)
+        {
+            bestTime = surviveTime;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        recordText.text = "Best Time : " + bestTime.ToString("F2");
+    }
+}
